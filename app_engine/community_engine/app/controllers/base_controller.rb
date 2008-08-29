c66487c42bc07d6537083ea765d54d5818c9a244
Jason Torres <jason.e.torres@gmail.com>
2008-08-29 13:07:55 +0800

    turn off caching for homepage

--- app_engine/community_engine/app/controllers/base_controller.rb
+++ app_engine/community_engine/app/controllers/base_controller.rb
@@ -6,8 +6,10 @@ class BaseController < ApplicationController
   include AuthenticatedSystem
   before_filter :login_from_cookie  
   skip_before_filter :verify_authenticity_token, :only => :footer_content
-    
-  caches_action :site_index, :footer_content, :if => Proc.new{|c| c.cache_action? }
+  
+  caches_action :footer_content, :if => Proc.new{|c| c.cache_action? }
+  
+  #caches_action :site_index, :footer_content, :if => Proc.new{|c| c.cache_action? }
   def cache_action?
     !logged_in? && controller_name.eql?('base') && params[:format].blank? 
   end  
